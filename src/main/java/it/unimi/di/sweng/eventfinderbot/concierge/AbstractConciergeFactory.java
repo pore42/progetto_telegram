e29d18537677a79792b083c94c1a4c1a1362be9d
Riccardo Bianchi <contervis@gmail.com>
2016-06-02 16:50:13 +0200

    Modificata factory concierge per contenere istanza IConcierge da personalizzare

--- src/main/java/it/unimi/di/sweng/eventfinderbot/concierge/AbstractConciergeFactory.java
+++ src/main/java/it/unimi/di/sweng/eventfinderbot/concierge/AbstractConciergeFactory.java
@@ -3,7 +3,9 @@ package it.unimi.di.sweng.eventfinderbot.concierge;
 /**
  * Created by istopped on 02/06/16.
  */
-public interface AbstractConciergeFactory {
+public abstract class AbstractConciergeFactory {
 
-    IConcierge instance();
+    private IConcierge concierge;
+
+    public abstract IConcierge instance();
 }
