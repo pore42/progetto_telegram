0b9e9d2e1d4443ef0aa6663fdca0b7ab1f55d541
Imran <imran.azam@studenti.unimi.it>
2016-06-09 15:46:03 +0200

    REFACTORING: estrazione metodi per isolare meglio le varie componenti coinvolte nel update e renderlo piu leggibile

--- src/main/java/it/unimi/di/sweng/eventfinderbot/webhook/BotResource.java
+++ src/main/java/it/unimi/di/sweng/eventfinderbot/webhook/BotResource.java
@@ -15,29 +15,51 @@ import java.util.List;
 
 public class BotResource extends ServerResource {
 
-	final TelegramBot bot = TelegramBotAdapter.build(Configs.INSTANCE.BOT_TOKEN);
-
 	@Post
 	public Representation update(Representation data) throws IOException {
 
-		final String token = getAttribute("token");
-		if (!Configs.INSTANCE.SERVER_TOKEN.equals(token)) {
-			setStatus(Status.CLIENT_ERROR_FORBIDDEN, "Worng server token");
+		if (!isServerTokenValid())
 			return null;
-		}
 
-		final Update update = BotUtils.parseUpdate(data.getText());
+		final Update update = getUpdate(data);
 
-		if (update.updateId() == null) {
-			getLogger().warning("Can't parse update, text was: \"" + data.getText() + "\"");
-			setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "Can't parse the update");
-			return null;
-		}
+		if(update != null)
+			processMessage(update);
+
+		return null;
+	}
+
+	private void processMessage(Update update) {
+
+		final TelegramBot bot = TelegramBotAdapter.build(Configs.INSTANCE.BOT_TOKEN);
 
 		RequestProcessor requestProcessor = new RequestProcessor(update);
+
 		List<SendMessage> messagesList = requestProcessor.process();
-		for(SendMessage toSend : messagesList)
-				bot.execute(toSend);
+		for (SendMessage toSend : messagesList)
+            bot.execute(toSend);
+	}
+
+	private boolean isServerTokenValid(){
+
+		final String token = getAttribute("token");
+		if (!Configs.INSTANCE.SERVER_TOKEN.equals(token))
+			return true;
+
+		setStatus(Status.CLIENT_ERROR_FORBIDDEN, "Wrong server token");
+
+		return false;
+	}
+
+	private Update getUpdate(Representation data) throws IOException {
+
+		Update update = BotUtils.parseUpdate(data.getText());
+
+		if(update != null)
+			return update;
+
+		getLogger().warning("Can't parse update, text was: \"" + data.getText() + "\"");
+		setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "Can't parse the update");
 
 		return null;
 	}
