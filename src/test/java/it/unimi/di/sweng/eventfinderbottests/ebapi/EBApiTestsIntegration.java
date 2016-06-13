7302387a20357556e3d73dca5a847f0fac1a62bf
pore <mollami@now.com>
2016-06-13 17:09:45 +0200

    VERDE: aggiunto test d'integrazione, con controllo su data

--- src/test/java/it/unimi/di/sweng/eventfinderbottests/ebapi/EBApiTestsIntegration.java
+++ src/test/java/it/unimi/di/sweng/eventfinderbottests/ebapi/EBApiTestsIntegration.java
@@ -7,8 +7,11 @@ import it.unimi.di.sweng.eventfinderbot.model.Event;
 import it.unimi.di.sweng.eventfinderbot.model.Location;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import static org.junit.Assert.*;
 public class EBApiTestsIntegration {
@@ -21,4 +24,26 @@ public class EBApiTestsIntegration {
         List<Event> events = api.getEvents(milano, new Date());
         assertTrue(events.size() >= 0 && events.size() <= Configs.INSTANCE.EBAPI_LIMIT());
     }
+    
+    @Test
+    public void testOrari()
+    {
+    	IEbApi api = new ConcreteEBApi();
+    	
+    	Date d = new Date();
+    	TimeZone tz = TimeZone.getTimeZone("UTC");
+		DateFormat df = new SimpleDateFormat("YYYY-MM-dd'T'hh:mm:ss");
+		df.setTimeZone(tz);
+		String data = df.format(d);
+    	
+    	List<Event> events = api.getEvents("milano", d);
+
+    	for(int i=0; i<events.size(); i++)
+    		assertTrue(    0 <=  (events.get(i).getStart().compareTo(data)));
+ 
+    	
+    }
+    
+    
+    
 }
