3051a83355099e9491f77615d3a95766a2eeb106
Nicolò Ciraci <nicolo.ciraci@studenti.unimi.it>
2016-06-15 15:24:45 +0200

    VERDE: implemtata request details

--- src/main/java/it/unimi/di/sweng/eventfinderbot/concierge/Concierge.java
+++ src/main/java/it/unimi/di/sweng/eventfinderbot/concierge/Concierge.java
@@ -2,6 +2,7 @@ package it.unimi.di.sweng.eventfinderbot.concierge;
 
 import it.unimi.di.sweng.eventfinderbot.model.*;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -27,6 +28,16 @@ public class Concierge implements IConcierge {
 
             current = new Response(content, Response.ResponseType.HERE_AND_NOW, request.getChatId());
         }
+		else if (request instanceof RequestDetails) {
+
+			try {
+				Event event = lastResponse.getContent().get(((RequestDetails) request).getIndex());
+				current = new Response(Arrays.asList(event), Response.ResponseType.EVENT_DETAILS, request.getChatId());
+			}
+			catch (Exception e) {
+				throw new IllegalStateException();
+			}
+		}
 
         if (current == null) throw new IllegalStateException();
 
