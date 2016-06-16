933c879e3b36afe0bc61b1e68bb2af2a2d4a2c84
Nicolò Ciraci <nicolo@150up.com>
2016-06-16 16:08:15 +0200

    REFACTORING: implementati model per requestfind

---
+++ src/main/java/it/unimi/di/sweng/eventfinderbot/model/RequestSetDate.java
@@ -0,0 +1,20 @@
+package it.unimi.di.sweng.eventfinderbot.model;
+
+import java.util.Date;
+
+/**
+ * Created by istopped on 16/06/16.
+ */
+public class RequestSetDate extends Request {
+
+	private Date date;
+
+	protected RequestSetDate(long chatId, Date date) {
+		super(chatId);
+		this.date = date;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+}
